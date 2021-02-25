76dbed8632957996538dab8edb62367771c4b405
Duncan Robertson <duncan@writeinstone.com>
2021-02-25 23:31:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using JsonBugReport.Models;
+using System;
+using System.Text.Json;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace JsonBugReport
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var session = new Session
+			{
+				SessionId = Guid.NewGuid(),
+				Created = DateTime.UtcNow,
+				Title = "Test"
+			};
+
+			var content = JsonContent.Create(session);
+			Task<string> task = content.ReadAsStringAsync();
+			task.Wait();
+			var json = task.Result;
+
+			Console.WriteLine("Serialised: ");
+			Console.WriteLine(json);
+
+			var deserialised = JsonSerializer.Deserialize<Session>(json);
+
+			Console.WriteLine();
+			Console.WriteLine("Deserialised: ");
+			Console.WriteLine(JsonSerializer.Serialize(deserialised));
+		}
+	}
+}
---
+++ Session.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace JsonBugReport.Models
+{
+	public partial class Session
+	{
+		public Guid SessionId { get; set; }
+		public string Title { get; set; }
+		public int Index { get; set; }
+		public string PublishedStreamUrl { get; set; }
+		public DateTime Created { get; set; }
+		public DateTime Uploaded { get; set; }
+		public DateTime LastModified { get; set; }
+		public string Version { get; set; }
+
+		public Guid ProjectId { get; set; }
+	}
+}
